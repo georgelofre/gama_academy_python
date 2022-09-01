c1278959d31ad4db2e0ac8cfb15bf39eceb68039
unknown <geo_freire@magazineluiza.intranet>
2022-09-01 16:04:06 -0300

    Alteração no 14

--- 01_EstruturaSequencial/Q014.py
+++ 01_EstruturaSequencial/Q014.py
@@ -1,4 +1,4 @@
-peso = float(input('Digite o peso em kg: '))
+peso = float(input('Digite o peso em kg se foi maior que 50kg: '))
 excesso = peso - 50
 multa = excesso * 4.0
 print(f'A quantidade em excesso é de {excesso:.2f}kg e a multa é de R$ {multa:.2f}.')
